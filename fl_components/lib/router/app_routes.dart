e865bd537736322cf989d3d60092d6b4dd9c5586
johssalinas <johssalinas@wposs.com>
2023-01-26 23:45:21 -0500

    Se agregaron las rutas oara la navegacion

---
+++ fl_components/lib/router/app_routes.dart
@@ -0,0 +1,49 @@
+import 'package:fl_components/models/models.dart';
+import 'package:flutter/material.dart';
+
+import '../screens/screens.dart';
+
+class AppRoutes {
+  static const initiaRoute = 'home';
+
+  static final menuOptions = <MenuOption>[
+    //TODO: borrar home
+    MenuOption(
+        route: 'home',
+        icon: Icons.home,
+        name: 'Home Screen',
+        screen: const HomeScreen()),
+    MenuOption(
+        route: 'listview1',
+        icon: Icons.list,
+        name: 'ListView tipo 1',
+        screen: const ListView1Screen()),
+    MenuOption(
+        route: 'listview2',
+        icon: Icons.list_alt,
+        name: 'ListView tipo 2',
+        screen: const ListView2Screen()),
+    MenuOption(
+        route: 'alert',
+        icon: Icons.add_alert,
+        name: 'Alertas',
+        screen: const AlertScreen()),
+    MenuOption(
+        route: 'card',
+        icon: Icons.card_giftcard,
+        name: 'Cards',
+        screen: const CardScreen()),
+  ];
+
+  static Map<String, Widget Function(BuildContext)> routes = {
+    'home': (BuildContext context) => const HomeScreen(),
+    'listview1': (BuildContext context) => const ListView1Screen(),
+    'listview2': (BuildContext context) => const ListView2Screen(),
+    'alrt': (BuildContext context) => const AlertScreen(),
+    'card': (BuildContext context) => const CardScreen(),
+  };
+
+  static Route<dynamic> onGenerateRoute(RouteSettings settings) {
+    return MaterialPageRoute(builder: (context) => const AlertScreen());
+  }
+}
